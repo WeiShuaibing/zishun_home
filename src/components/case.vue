84e3826a6e0a2e646ac3edd02c5c2e1ee6371d45
weicomeon <weishuaibing666@qq.com>
2018-11-08 17:54:08 +0800

    首页OK

--- src/components/case.vue
+++ src/components/case.vue
@@ -1,20 +1,152 @@
 <template>
-  <div>
-    <h2>案例展示测试</h2>
-    <p v-html="test"></p>
-    <img :src='desc'/>
-    <h3>test</h3>
+  <div class="main">
+    <div class="one">
+      首页 > 案例展示
+    </div>
+    <div class="two">
+      <div class="row1">
+        <div class="row1_left">案例户型：</div>
+        <div class="row1_right">
+          <ul>
+            <li>全部</li>
+            <li>别墅</li>
+            <li>跃层</li>
+            <li>普通住宅</li>
+            <li>工装</li>
+            <li>二居室</li>
+            <li>三居室</li>
+            <li>四居室</li>
+            <li>Loft</li>
+            <li>复式</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row2">
+        <div class="row2_left">装修风格：</div>
+        <div class="row2_right">
+          <ul>
+            <li>全部</li>
+            <li>现代简约</li>
+            <li>简欧</li>
+            <li>新中式</li>
+            <li>北欧风格</li>
+            <li>简美风格</li>
+            <li>新古典</li>
+            <li>雅致主义</li>
+            <li>美式乡村</li>
+            <li>现代前卫</li>
+            <li>欧式古典</li>
+            <li>地中海风格</li>
+            <li>其他</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row3">
+        <div class="row3_left">房屋面积：</div>
+        <div class="row3_right">
+          <ul>
+            <li>全部</li>
+            <li>80-100平米</li>
+            <li>100-140平米</li>
+            <li>140-200平米</li>
+            <li>200-320平米</li>
+            <li>320-500平米</li>
+            <li>500-1000平米</li>
+            <li>1000平米以上</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="three">
+      <div class="three_item">
+        <img :src="imgsrc" alt="点击查看详情" class="item_img"/>
+        <div class="item_text">
+          <div class="item_text_1">
+            泰宏建业国际城130平北欧风格
+          </div>
+          <div class="item_text_2">
+            普通住宅 | 北欧风格 | 80-100平米
+          </div>
+        </div>
+      </div>
+      <div class="three_item">
+        <img :src="imasrc2" alt="点击查看详情" class="item_img"/>
+        <div class="item_text">
+          <div class="item_text_1">
+            泰宏建业国际城130平北欧风格
+          </div>
+          <div class="item_text_2">
+            普通住宅 | 北欧风格 | 80-100平米
+          </div>
+        </div>
+      </div>
+      <div class="three_item">
+        <img :src="imgsrc" alt="点击查看详情" class="item_img"/>
+        <div class="item_text">
+          <div class="item_text_1">
+            泰宏建业国际城130平北欧风格
+          </div>
+          <div class="item_text_2">
+            普通住宅 | 北欧风格 | 80-100平米
+          </div>
+        </div>
+      </div>
+      <div class="three_item">
+        <img :src="imasrc2" alt="点击查看详情" class="item_img"/>
+        <div class="item_text">
+          <div class="item_text_1">
+            泰宏建业国际城130平北欧风格
+          </div>
+          <div class="item_text_2">
+            普通住宅 | 北欧风格 | 80-100平米
+          </div>
+        </div>
+      </div>
+      <div class="three_item">
+        <img :src="imasrc2" alt="点击查看详情" class="item_img"/>
+        <div class="item_text">
+          <div class="item_text_1">
+            泰宏建业国际城130平北欧风格
+          </div>
+          <div class="item_text_2">
+            普通住宅 | 北欧风格 | 80-100平米
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!--清除浮动效果-->
+    <div style="clear: both;" />
+    <div class="changepage">
+      <div style="margin-top: 25px;">
+        <pagination
+          background
+          layout="prev, pager, next"
+          :page-size="pagesize"
+          :total='total'
+          @size-change="sizechange"
+          @current-change="currentchange"
+          @prev-click="prevclick"
+          @next-click="nextclick"
+        >
+        </pagination>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Pagination } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 export default {
   name: 'case', // 案例展示
+  components: { Pagination },
   data () {
     return {
-      caseList: null,
-      test: '',
-      desc: ''
+      imgsrc: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1541593284&di=da5fa11a65ac1c6130a3b1b4e66f96ad&src=http://imgsrc.baidu.com/imgad/pic/item/962bd40735fae6cd79fe484604b30f2442a70f4a.jpg',
+      imasrc2: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541604198502&di=1b59bea4218eb574489549bee923828e&imgtype=0&src=http%3A%2F%2Fpic10.photophoto.cn%2F20090107%2F0033034157010008_b.jpg',
+      pagesize: 12,
+      total: 1000
     }
   },
   mounted () {
@@ -26,14 +158,22 @@ export default {
         url: '/api/admin/case/getCaseList',
         methods: 'get'
       }).then(res => {
-        console.log(res.data.data[0].content)
-        console.log(123)
-        console.log(this.htmlDecodeByRegExp(res.data.data[4].content))
-        this.test = this.htmlDecodeByRegExp(res.data.data[4].content)
-        this.desc = this.htmlDecodeByRegExp(res.data.data[4].cover)
+        console.log(res)
       }).catch(err => {
         console.log(err)
       })
+    },
+    sizechange (val) {
+      console.log('每页条数' + val)
+    },
+    prevclick (val) {
+      console.log('前一页：' + val)
+    },
+    nextclick (val) {
+      console.log('下一页' + val)
+    },
+    currentchange (val) {
+      console.log('当前页' + val)
     },
     htmlDecodeByRegExp (str) {
       var s = ''
@@ -51,4 +191,146 @@ export default {
 </script>
 
 <style scoped  lang="less">
+.main{
+  font-family: -apple-system,SF UI Text,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,sans-serif;
+  margin: 0 auto;
+  /*border: 1px solid red;*/
+  font-size: 16px;
+  .one{
+    width: 1200px;
+    margin: 0 auto;
+    height: 50px;
+    font-size: 14px;
+    line-height: 4;
+  }
+  .two{
+    width: 1200px;
+    margin: 0 auto;
+    line-height: 55px;
+    text-align: center;
+    border: 2px solid #DDDDDD;
+    .row1{
+      height: 55px;
+      border-bottom: 2px solid #DDDDDD;
+      .row1_left{
+        float: left;
+        width: 126px;
+        background-color: #F5F5F5;
+        border-right: 2px solid #DDDDDD;
+      }
+      .row1_right{
+        float: right;
+        width: 1000px;
+        margin-right: 40px;
+        ul{
+          text-align: left;
+        }
+        li{
+          font-size: 14px;
+          margin-left: 18px;
+          margin-right: 18px;
+          list-style: none;
+          display: inline;
+        }
+      }
+    }
+    .row2{
+      height: 110px;
+      border-bottom: 2px solid #DDDDDD;
+      .row2_left{
+        float: left;
+        width: 126px;
+        height: 100%;
+        background-color: #F5F5F5;
+        border-right: 2px solid #DDDDDD;
+      }
+      .row2_right{
+        float: right;
+        width: 1000px;
+        margin-right: 40px;
+        ul{
+          text-align: left;
+        }
+        li{
+          font-size: 14px;
+          margin-left: 18px;
+          margin-right: 18px;
+          list-style: none;
+          display: inline;
+        }
+      }
+    }
+    .row3{
+      height: 55px;
+      .row3_left{
+        float: left;
+        width: 126px;
+        background-color: #F5F5F5;
+        border-right: 2px solid #DDDDDD;
+      }
+      .row3_right{
+        float: right;
+        width: 1000px;
+        margin-right: 40px;
+        /*background-color: aqua;*/
+        ul{
+          text-align: left;
+        }
+        li{
+          font-size: 14px;
+          margin-left: 18px;
+          margin-right: 18px;
+          list-style: none;
+          display: inline;
+        }
+      }
+    }
+  }
+  .three{
+    margin: 0 auto;
+    width: 1224px;
+    /*height: 600px;*/
+    .three_item{
+      overflow: hidden;
+      margin-top: 30px;
+      float: left;
+      margin-left: 12px;
+      margin-right: 12px;
+      .item_img{
+        cursor: pointer;
+        transition: all 0.6s;
+        width: 282px;
+        height: 240px;
+      }
+      .item_img:hover{
+        transform: scale(1.1);
+      }
+      .item_text{
+        box-shadow:0px 2px 6px 0px rgba(171,171,171,0.2);
+        .item_text_1{
+          font-size: 14px;
+          color: #333333;
+          line-height:30px;
+          padding-left: 5px;
+        }
+        .item_text_2{
+          font-size: 14px;
+          color: #999999;
+          line-height:30px;
+          padding-left: 5px;
+        }
+      }
+    }
+    /*.three_item:hover{*/
+      /*transform: scale(1.1);*/
+    /*}*/
+  }
+  .changepage{
+    height: 80px;
+    width: 1200px;
+    margin: 0 auto;
+    text-align: center;
+  }
+}
+
 </style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,334 @@
 <template>
-  <div>
-    <h2>首页</h2>
+  <div class="main">
+    <div class="face">
+      <img :src="img.imgface" class="face_img" />
+    </div>
+    <div class="advantage">
+      <div class="advantage_text">我们的优势</div>
+      <div class="sandian">
+        <div class="sandian_item">
+          <div style="width:136px;margin: 0 auto;">
+            <img :src="img.xiaolv" class="xiaolv" />
+            <span class="sandian_title">效率</span>
+          </div>
+          <div class="sandian_detail">
+            <span>精湛的施工工艺，完美的专业技术；精湛的施工工艺</span>
+          </div>
+        </div>
+        <div class="sandian_item">
+          <div style="width:136px;margin: 0 auto;">
+            <img :src="img.gongyi" class="xiaolv" />
+            <span class="sandian_title">效率</span>
+          </div>
+          <div class="sandian_detail">
+            <span>精湛的施工工艺，完美的专业技术；精湛的施工工艺，完美的专业技术</span>
+          </div>
+        </div>
+        <div class="sandian_item">
+          <div style="width:136px;margin: 0 auto;">
+            <img :src="img.jiage" class="xiaolv" />
+            <span class="sandian_title">效率</span>
+          </div>
+          <div class="sandian_detail">
+            <span>精湛的施工工艺，完美的专业技术；精湛的施工工艺，完美</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="teams">
+      <div class="teams_text">优秀的团队</div>
+      <div class="teams_img">
+        <div class="teams_img_item">
+          <div style="width: 282px;margin: 0 auto;overflow: hidden;">
+            <img :src="img.teams1" class="teams_img_item_img" />
+          </div>
+          <div style="height: 70px;width:282px;margin:0 auto;background:rgba(245,245,245,1);text-align: center;">
+            <div style="line-height: 65px;">
+              <span style="font-size:20px;line-height:28px;">高凡</span>
+              <span style="font-size:14px;line-height:20px;">高级设计师</span>
+            </div>
+          </div>
+        </div>
+        <div class="teams_img_item">
+          <div style="width: 282px;margin: 0 auto;overflow: hidden;">
+            <img :src="img.teams2" class="teams_img_item_img" />
+          </div>
+          <div style="height: 70px;width:282px;margin:0 auto;background:rgba(245,245,245,1);text-align: center;">
+            <div style="line-height: 65px;">
+              <span style="font-size:20px;line-height:28px;">高凡</span>
+              <span style="font-size:14px;line-height:20px;">设计总监</span>
+            </div>
+          </div>
+        </div>
+        <div class="teams_img_item">
+          <div style="width: 282px;margin: 0 auto;overflow: hidden;">
+            <img :src="img.teams3" class="teams_img_item_img" />
+          </div>
+          <div style="height: 70px;width:282px;margin:0 auto;background:rgba(245,245,245,1);text-align: center;">
+            <div style="line-height: 65px;">
+              <span style="font-size:20px;line-height:28px;">高凡</span>
+              <span style="font-size:14px;line-height:20px;">高级设计师</span>
+            </div>
+          </div>
+        </div>
+        <div class="teams_img_item">
+          <div style="width: 282px;margin: 0 auto;overflow: hidden;">
+            <img :src="img.teams4" class="teams_img_item_img" />
+          </div>
+          <div style="height: 70px;width:282px;margin:0 auto;background:rgba(245,245,245,1);text-align: center;">
+            <div style="line-height: 65px;">
+              <span style="font-size:20px;line-height:28px;">高凡</span>
+              <span style="font-size:14px;line-height:20px;">设计总监</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="business">
+      <div class="business_text">我们的业务</div>
+      <div class="sikuai">
+        <div class="row1">
+          <div class="row1_left">
+            <div class="row1_left_text">全屋定制</div>
+            <div class="row1_left_text2">
+              精湛的施工工艺，完美的专业技术；
+              精湛的施工工艺。精湛的施工工艺，完美的专业技术；精湛的施工工艺。
+            </div>
+          </div>
+          <div class="row1_right">
+            <img :src="img.row1" class="row1_right_img">
+          </div>
+        </div>
+        <div class="row2">
+          <div class="row2_left">
+            <img :src="img.row2" class="row2_left_img">
+          </div>
+          <div class="row2_right">
+            <div class="row2_right_text">智能家居</div>
+            <div class="row2_right_text2">
+              精湛的施工工艺，完美的专业技术；
+              精湛的施工工艺。精湛的施工工艺，完美的专业技术；精湛的施工工艺。
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="height: 50px;"></div>
   </div>
 </template>
 
 <script>
+import imgface from '../assets/image/face.png'
+import xiaolv from '../assets/image/xiaolv.png'
+import gongyi from '../assets/image/gongyi.png'
+import jiage from '../assets/image/jiage.png'
+import teams1 from '../assets/image/teams1.png'
+import teams2 from '../assets/image/teams2.png'
+import teams3 from '../assets/image/teams3.png'
+import teams4 from '../assets/image/teams4.png'
+import row1 from '../assets/image/row1.png'
+import row2 from '../assets/image/row2.png'
+
 export default {
-  name: 'home' // 首页
+  name: 'home', // 首页
+  data () {
+    return {
+      img: {
+        imgface,
+        xiaolv,
+        gongyi,
+        jiage,
+        teams1,
+        teams2,
+        teams3,
+        teams4,
+        row1,
+        row2
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.main{
+  font-family: -apple-system,SF UI Text,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,sans-serif;
+  font-size: 16px;
+  margin: 0 auto;
+  .face{
+    width: 1920px;
+    margin: 0 auto;
+    .face_img{
+      width: 1920px;
+      height: 860px;
+    }
+  }
+  .advantage{
+    width: 1200px;
+    margin: 0 auto;
+    .advantage_text{
+      width:210px;
+      font-size:42px;
+      font-weight:500;
+      line-height:4;
+      text-align: center;
+      margin: 0 auto;
+    }
+    .sandian{
+      height: 350px;
+      display: flex;
+      .sandian_item{
+        flex: 1;
+        text-align: center;
+        .xiaolv{
+          width: 136px;
+          height: 136px;
+        }
+        .sandian_title{
+          font-size:30px;
+          font-weight:500;
+          color:rgba(51,51,51,1);
+          line-height: 3;
+        }
+        .sandian_detail{
+          margin: 0 auto;
+          width:308px;
+          height:56px;
+          font-size:20px;
+          line-height:28px;
+        }
+      }
+    }
+  }
+  .teams{
+    padding-bottom: 30px;
+    width: 1200px;
+    margin: 0 auto;
+    .teams_text{
+      width:210px;
+      font-size:42px;
+      font-weight:500;
+      line-height:4;
+      text-align: center;
+      margin: 0 auto;
+    }
+    .teams_img{
+      display: flex;
+      .teams_img_item{
+        flex: 1;
+        height: 370px;
+        justify-content:space-between;
 
+        .teams_img_item_img{
+          margin: 0 auto;
+          width:282px;
+          height: 300px;
+          transition: all 0.6s;
+        }
+        .teams_img_item_img:hover{
+          transform: scale(1.2);
+        }
+      }
+    }
+  }
+  .business{
+    width: 1200px;
+    margin: 0 auto;
+    .business_text{
+      width:210px;
+      font-size:42px;
+      font-weight:500;
+      line-height:4;
+      text-align: center;
+      margin: 0 auto;
+    }
+    .sikuai{
+      width: 1200px;
+      height: 780px;
+      margin: 0 auto;
+      .row1{
+        height: 390px;
+        .row1_left{
+          height: 100%;
+          float: left;
+          width: 600px;
+          background-color: #8062EE;
+          .row1_left_text{
+            font-size:42px;
+            line-height:4;
+            font-weight:800;
+            font-family:PingFang-SC-Heavy;
+            color: white;
+            width: 184px;
+            margin: 0 auto;
+          }
+          .row1_left_text2{
+            margin: 0 auto;
+            width:415px;
+            height:108px;
+            font-size:22px;
+            font-family:PingFang-SC-Medium;
+            font-weight:500;
+            color:rgba(255,255,255,1);
+            line-height:36px;
+          }
+        }
+        .row1_right{
+          float: right;
+          height: 100%;
+          width: 600px;
+          overflow: hidden;
+          .row1_right_img{
+            width: 600px;
+            height: 390px;
+            transition: all 0.6s;
+          }
+          .row1_right_img:hover{
+            transform: scale(1.2);
+          }
+        }
+      }
+      .row2{
+        height: 390px;
+        .row2_left{
+          height: 100%;
+          float: left;
+          width: 600px;
+          overflow: hidden;
+          .row2_left_img{
+            width: 600px;
+            height: 390px;
+            transition: all 0.6s;
+          }
+          .row2_left_img:hover{
+            transform: scale(1.2);
+          }
+        }
+        .row2_right{
+          float: right;
+          height: 100%;
+          width: 600px;
+          background-color: rgba(245,245,245,1);
+          .row2_right_text{
+            font-size:42px;
+            line-height:4;
+            font-weight:800;
+            font-family:PingFang-SC-Heavy;
+            width: 184px;
+            margin: 0 auto;
+          }
+          .row2_right_text2{
+            margin: 0 auto;
+            width:415px;
+            height:108px;
+            font-size:22px;
+            font-family:PingFang-SC-Medium;
+            font-weight:500;
+            line-height:36px;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
